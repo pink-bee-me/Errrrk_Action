17961e0f35c3b579950e8692db4944845aff6d73
pink-bee-me <pink.bee.me@outlook.com>
2025-01-22 15:42:42 -0600

    Trying to get Active link to Work

--- JavaScript/ActiveClassImplementation.js
+++ JavaScript/ActiveClassImplementation.js
@@ -1,15 +1,6 @@
 
 // get the container element
-var navBar = document.getElementById("#navBar");
 
 //get all the buttons (or <a> link tags) inside the container
-var btns= navList.getElementsByClassName(".btn");
 
-// loop through the buttons and add the active class to the current/clicked button
-for(var i=0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";    
-});
-}
+    
